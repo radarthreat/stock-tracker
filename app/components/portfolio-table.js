c26d89124d158ffca8f7059c7aac0675f23283b9
radarthreat <paul.sethre@gmail.com>
2018-04-30 21:32:37 -0500

    Setup table and model

--- app/components/portfolio-table.js
+++ app/components/portfolio-table.js
@@ -1,4 +1,32 @@
 import Component from '@ember/component';
+import { computed } from '@ember/object';
+import TableCommon from '../mixins/table-common';
 
-export default Component.extend({
+export default Component.extend(TableCommon, {
+    columns: computed(function() {
+        return [{
+          label: 'Avatar',
+          valuePath: 'avatar',
+          width: '60px',
+          sortable: false,
+          cellComponent: 'user-avatar'
+        }, {
+          label: 'First Name',
+          valuePath: 'firstName',
+          width: '150px'
+        }, {
+          label: 'Last Name',
+          valuePath: 'lastName',
+          width: '150px'
+        }, {
+          label: 'Address',
+          valuePath: 'address'
+        }, {
+          label: 'State',
+          valuePath: 'state'
+        }, {
+          label: 'Country',
+          valuePath: 'country'
+        }];
+      })
 });
--- app/models/portfolio.js
+++ app/models/portfolio.js
@@ -1,5 +1,8 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-
-});
+    symbol: DS.attr(),
+    price: DS.attr('number'),
+    trend: DS.attr(),
+    volume: DS.attr('number'),
+}); 
